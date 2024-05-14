694613991b7d09e3fdb110b91abef09eb2ed2a84
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2024-05-14 14:10:33 -0400

    doing same timings as ze and hsa (recording size + latencies of 7 core api calls)

--- benchmarking/cuda/memMapTest.c
+++ benchmarking/cuda/memMapTest.c
@@ -3,8 +3,12 @@
 #include <time.h>
 #include <cuda.h>
 
-uint64_t round_up_mult_pow_two(uint64_t orig, uint64_t multiple){
-	return (orig + multiple - 1) & (~(multiple - 1));
+uint64_t get_aligned_size(uint64_t obj_size, uint64_t page_size){
+	int remainder = obj_size % page_size;
+    if (remainder == 0)
+        return obj_size;
+
+    return obj_size + page_size - remainder;
 }
 
 int init_driver(uint64_t init_flags){
@@ -56,6 +60,15 @@ CUmemAllocationProp set_default_alloc_prop(int device_id){
 	return prop;
 
 }
+
+
+// HARDCODE TO 2 MB
+// Intel has 2 different min granualrities (64K and 2MB) depending on alloc size
+uint64_t get_min_alloc_granularity(int device_id) {
+	return 1 << 21;
+}
+
+
 
 // ASSUMES CONTEXT FOR DEVICE WAS SET
 // Using default allocation properties
@@ -156,6 +169,57 @@ int set_access(int device_id, uint64_t va_range_size, void * va_ptr){
 }
 
 
+int mem_unmap(void * va_ptr, uint64_t map_size){
+	
+	CUresult result;
+	const char * err;
+
+    result = cuMemUnmap((CUdeviceptr) va_ptr, map_size);
+    if (result != CUDA_SUCCESS){
+    	cuGetErrorString(result, &err);
+        fprintf(stderr, "Error: could not do unmapping: %d\n", result);
+        return -1;
+    }
+    return 0;
+}
+
+// Freeing before unmapping also does unmapping (but doesn't destroy physical allocations)
+int free_va_space(void * va_ptr, uint64_t va_range_size) {
+	CUresult result;
+	const char * err;
+
+    result = cuMemAddressFree((CUdeviceptr) va_ptr, va_range_size);
+    if (result != CUDA_SUCCESS){
+    	cuGetErrorString(result, &err);
+        fprintf(stderr, "Error: could not free va space: %d\n", result);
+        return -1;
+    }
+    return 0;
+}
+
+int release_phys_mem(CUmemGenericAllocationHandle phys_mem_handle){
+	CUresult result;
+	const char * err;
+    result = cuMemRelease(phys_mem_handle);
+    if (result != CUDA_SUCCESS){
+    	cuGetErrorString(result, &err);
+        fprintf(stderr, "Error: could not release phys memory: %d\n", result);
+        return -1;
+    }
+    return 0;
+}
+
+
+int close_shareable_handle(int dmabuf_fd) {
+
+	// Think thi is ONLY supported for zeMemAllocDevice and zeMemAllocHost
+	// and NOT zePhysicalMemCreate + zeVirtualMemReservev + zeVirtualMemMap
+
+	fprintf(stderr, "Unimplemented err: close_shareable_handle not implemented\n");
+
+}
+
+
 int main(int argc, char *argv[]) {
 
 
@@ -178,29 +242,24 @@ int main(int argc, char *argv[]) {
     // Setting default properties
   	CUmemAllocationProp prop = set_default_alloc_prop(device_id);
 
-    // Get min chunk granularity   
-	uint64_t granularity = 0;
-	ret = get_min_chunk_granularity(device_id, &granularity, &prop);
-	if (ret != 0){
-		exit(1);
-	}
+    
 
-	// Perform phys mem allocation
+	// Setting alloc size to input argument
+	uint64_t raw_alloc_size = (uint64_t) atol(argv[1]);
+	
+	// Get min chunk granularity  
+    // hardcoding to 1 << 21
+	uint64_t min_granularity = get_min_alloc_granularity(device_id);
+	uint64_t alloc_size = get_aligned_size(raw_alloc_size, min_granularity);
 
-	// Setting alloc size to 1 gpu page
-	uint64_t raw_alloc_size = (uint64_t) 1 * (1 << 21);
 	
 
-
-	// assumes granularity is power of 2...
-	uint64_t alloc_size = round_up_mult_pow_two(raw_alloc_size, granularity);
-
-	printf("DOING TIMINGS WITH ALLOC SIZE = %ld\n\n", alloc_size);
+	// Perform phys mem allocation
+	printf("%ld,", alloc_size);
 	
 
 	struct timespec start, stop;
 	uint64_t timestamp_start, timestamp_stop, elapsed;
-
 
 	// 1. Allocating Physical Memory
 	clock_gettime(CLOCK_REALTIME, &start);
@@ -209,44 +268,50 @@ int main(int argc, char *argv[]) {
 	CUmemGenericAllocationHandle phys_mem_handle;
 	ret = alloc_phys_chunk(alloc_size, &phys_mem_handle, &prop);
 	if (ret != 0){
+		printf("\n");
 		exit(1);
 	}
 
 	clock_gettime(CLOCK_REALTIME, &stop);
 	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
 	elapsed = timestamp_stop - timestamp_start;
-	printf("\tAlloc Phys Chunk Took: %ld\n", elapsed);
+	printf("%ld,", elapsed);
 
+
+	// * NOT DEALING WITH EXPORTING/IMPORTING YET
+	//	- NEED TO UNDERSTAND HOW TO GET DMABUF in Cuda!
 
 	// 2. Exporting to Shareable Handle
-	clock_gettime(CLOCK_REALTIME, &start);
-	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	// clock_gettime(CLOCK_REALTIME, &start);
+	// timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
 
-	void * shareable_phys_mem_handle;
-	ret = export_to_shareable_handle(phys_mem_handle, &shareable_phys_mem_handle);
-	if (ret != 0){
-		exit(1);
-	}
+	// void * shareable_phys_mem_handle;
+	// ret = export_to_shareable_handle(phys_mem_handle, &shareable_phys_mem_handle);
+	// if (ret != 0){
+	// 	printf("\n");
+	// 	exit(1);
+	// }
 
-	clock_gettime(CLOCK_REALTIME, &stop);
-	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
-	elapsed = timestamp_stop - timestamp_start;
-	printf("\tExport to Shareable Handle Took: %ld\n", elapsed);
+	// clock_gettime(CLOCK_REALTIME, &stop);
+	// timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	// elapsed = timestamp_stop - timestamp_start;
+	// printf("%ld,\n", elapsed);
 
 
-	// 3. Importing from Shareable Handle
-	clock_gettime(CLOCK_REALTIME, &start);
-	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	// // 3. Importing from Shareable Handle
+	// clock_gettime(CLOCK_REALTIME, &start);
+	// timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
 
-	CUmemGenericAllocationHandle imported_handle;
-	ret = import_from_shareable_handle(shareable_phys_mem_handle, &imported_handle);
-	if (ret != 0){
-		exit(1);
-	}
-	clock_gettime(CLOCK_REALTIME, &stop);
-	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
-	elapsed = timestamp_stop - timestamp_start;
-	printf("\tImporting From Shareable Handle Took: %ld\n", elapsed);
+	// CUmemGenericAllocationHandle imported_handle;
+	// ret = import_from_shareable_handle(shareable_phys_mem_handle, &imported_handle);
+	// if (ret != 0){
+	// 	printf("\n");
+	// 	exit(1);
+	// }
+	// clock_gettime(CLOCK_REALTIME, &stop);
+	// timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	// elapsed = timestamp_stop - timestamp_start;
+	// printf("%ld,", elapsed);
 
 
 	// 4. Reserving VA Space
@@ -257,12 +322,13 @@ int main(int argc, char *argv[]) {
 	uint64_t va_range_size = alloc_size;
 	ret = reserve_va_space(va_range_size, &va_ptr);
 	if (ret != 0){
+		printf("\n");
 		exit(1);
 	}
 	clock_gettime(CLOCK_REALTIME, &stop);
 	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
 	elapsed = timestamp_stop - timestamp_start;
-	printf("\tReserving VA Space Took: %ld\n", elapsed);
+	printf("%ld,", elapsed);
 
 
 	// 5. Doing memory mapping
@@ -270,14 +336,15 @@ int main(int argc, char *argv[]) {
 	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
 
 	uint64_t map_size = alloc_size;
-	ret = mem_map(map_size, va_ptr, imported_handle);
+	ret = mem_map(map_size, va_ptr, phys_mem_handle);
 	if (ret != 0){
+		printf("\n");
 		exit(1);
 	}
 	clock_gettime(CLOCK_REALTIME, &stop);
 	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
 	elapsed = timestamp_stop - timestamp_start;
-	printf("\tMemory Mapping Took: %ld\n", elapsed);
+	printf("%ld,", elapsed);
 
 
 	// 6. Setting Access
@@ -286,12 +353,83 @@ int main(int argc, char *argv[]) {
 	
 	ret = set_access(device_id, va_range_size, va_ptr);
 	if (ret != 0){
+		printf("\n");
 		exit(1);
 	}
 	clock_gettime(CLOCK_REALTIME, &stop);
 	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
 	elapsed = timestamp_stop - timestamp_start;
-	printf("\tSetting Access Took: %ld\n\n", elapsed);
+	printf("%ld,", elapsed);
+
+
+	// 7. Unmapping
+	clock_gettime(CLOCK_REALTIME, &start);
+	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	ret = mem_unmap(va_ptr, map_size);
+	if (ret != 0){
+		printf("\n");
+		exit(1);
+	}
+	clock_gettime(CLOCK_REALTIME, &stop);
+	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	elapsed = timestamp_stop - timestamp_start;
+	printf("%ld,", elapsed);
+
+
+	// 8. Freeing VA space
+	clock_gettime(CLOCK_REALTIME, &start);
+	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	ret = free_va_space(va_ptr, va_range_size);
+	if (ret != 0){
+		printf("\n");
+		exit(1);
+	}
+	clock_gettime(CLOCK_REALTIME, &stop);
+	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	elapsed = timestamp_stop - timestamp_start;
+	printf("%ld,", elapsed);
+
+	// NOTE
+	//	- physical memory is only freed upon explicit zePhysicalMemDestroy
+	//	- unclear when using the regualr zeMemAllocDevice + exporting to dmabuf / importing...
+
+	// 9. UNSUPPORTED: Close the imported dmabuf
+	// clock_gettime(CLOCK_REALTIME, &start);
+	// timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	// ret = close_shareable_handle(dmabuf_fd);
+	// if (ret != 0){
+	// 	exit(1);
+	// }
+	// clock_gettime(CLOCK_REALTIME, &stop);
+	// timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	// elapsed = timestamp_stop - timestamp_start;
+	// printf("%ld,", elapsed);
+
+	// 10. UNSUPPORTED: Release imported physical memory
+	// clock_gettime(CLOCK_REALTIME, &start);
+	// timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	// ret = release_phys_mem(imported_handle);
+	// if (ret != 0){
+	// 	exit(1);
+	// }
+	// clock_gettime(CLOCK_REALTIME, &stop);
+	// timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	// elapsed = timestamp_stop - timestamp_start;
+	// printf("%ld,", elapsed);
+
+	
+	// 11. Finally release the physical allocation which creates free page frames
+	clock_gettime(CLOCK_REALTIME, &start);
+	timestamp_start = start.tv_sec * 1e9 + start.tv_nsec;
+	ret = release_phys_mem(phys_mem_handle);
+	if (ret != 0){
+		printf("\n");
+		exit(1);
+	}
+	clock_gettime(CLOCK_REALTIME, &stop);
+	timestamp_stop = stop.tv_sec * 1e9 + stop.tv_nsec;
+	elapsed = timestamp_stop - timestamp_start;
+	printf("%ld\n", elapsed);
 
 	return 0;
 
